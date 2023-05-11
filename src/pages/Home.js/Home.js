351cd1c4ab8ab7e04685794bd239ee465cf28439
Muhammad Ahmed <62472263+ahmedazhar2299@users.noreply.github.com>
2023-05-11 18:52:14 +0500

    initial configuration files

--- src/pages/Home.js/Home.js
+++ src/pages/Home.js/Home.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Home = () => {
-  return <div></div>;
+  return (
+    <div className="text-green-500 text-center text-5xl text-bold capitalize">
+      Starter files for REACT Project
+    </div>
+  );
 };
 
 export default Home;
--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -1,17 +1,13 @@
 import React from "react";
-import { Navigate, Route, Routes, useNavigate } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Home from "../pages/Home.js/Home";
-import SignIn from "../pages/Registration/SignIn";
 import Error from "../pages/Error/Error";
-import SignUp from "../pages/Registration/SignUp";
 function App() {
   return (
     <div className="font-montserrat">
       <Routes>
         <Route path="/">
           <Route index element={<Home />} />
-          <Route path="signin" element={<SignIn />} />
-          <Route path="signup" element={<SignUp />} />
           <Route path="*" element={<Error />} />
         </Route>
       </Routes>
